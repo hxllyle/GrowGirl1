bd0a565ae8ffc02021d94da47d1087f6a3ba7d5e
Holly <simplyholly1989@gmail.com>
2020-08-12 11:47:25 -0700

    Initial Commit

---
+++ GrowGirl1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrowGirl1
+//
+//  Created by Holly Le on 8/12/20.
+//  Copyright © 2020 Holly Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
