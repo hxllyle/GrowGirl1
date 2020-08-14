6a946bfda56057d05e044429285a205b352c1b6f
Camila Esparza <esparza.camila.26@gmail.com>
2020-08-13 18:02:58 -0700

    talk to a  professional + depression pages

--- GrowGirl1/DiscussionViewController.swift
+++ GrowGirl1/DiscussionViewController.swift
@@ -24,8 +24,7 @@ class DiscussionViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
+    /*    // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
